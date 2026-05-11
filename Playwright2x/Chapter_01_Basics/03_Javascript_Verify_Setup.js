708f33fc26f4f97a76094720dea6a783789a8c80
Chetna23-code <toor.chetna@gmail.com>
2026-05-11 16:16:07 +0530

    Initial commit: Add Playwright automation code

---
+++ Playwright2x/Chapter_01_Basics/03_Javascript_Verify_Setup.js
@@ -0,0 +1,10 @@
+console.log(process.platform);
+// Winddows: WIN32
+//MAC: darwin
+//LINUX: LINUX
+
+console.log(process.arch);
+//windows: x64
+//MAC: arm64
+
+console.log("Node Version:", process.version);
